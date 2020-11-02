b7c1cd1ba74459b3ee284bf248e2e9ba3f866283
dashasaldin <66440260+dashasaldin@users.noreply.github.com>
2020-11-02 20:10:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const dataset = "GDP-data.json";
+
+d3.json(dataset, (error, data) => {
+    const w = 500;
+    const h = 500;
+    const padding = 60;
+
+    const xScale = d3.scaleLinear()
+                     .domain([0, d3.max(data.data, (d) => d[0])])
+                     .range([padding, w - padding]);
+
+    const yScale = d3.scaleLinear()
+                     .domain([0, d3.max(data.data, (d) => d[1])])
+                     .range([h - padding, padding]);
+
+    const svg = d3.select("body")
+                  .append("svg")
+                  .attr("width", w)
+                  .attr("height", h);
+
+    svg.selectAll("rect")
+       .data(data.data)
+       .enter().append("rect")
+       .attr("class", "bar")
+       .attr("x", (d) => xScale(d[0]))
+       .attr("y",(d) => yScale(d[1]))
+
+    svg.selectAll("text")
+       .data(data.data)
+       .enter()
+       .append("text")
+       .text((d) =>  (d[0] + "," + d[1]))
+       .attr("x", (d) => xScale(d[0] + 10))
+       .attr("y", (d) => yScale(d[1]))
+
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
+
+    svg.append("g")
+       .attr("transform", "translate(0," + (h - padding) + ")")
+       .call(xAxis);
+
+    svg.append("g")
+       .attr("transform", "translate(" + padding + ",0)")
+       .call(yAxis)
+             
+})
+
+
+
+    
